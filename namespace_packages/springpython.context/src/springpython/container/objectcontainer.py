f94a272f949eb8d19642e1d97ff7af2d6662f053
Sven Wilhelm <wilhelm@wiredobjects.eu>
2011-09-13 20:11:47 +0200

    Update license head, imports and format
    Signed-off-by: Sven Wilhelm <wilhelm@wiredobjects.eu>

--- namespace_packages/springpython.context/src/springpython/container/objectcontainer.py
+++ namespace_packages/springpython.context/src/springpython/container/objectcontainer.py
@@ -1,21 +1,27 @@
-"""
-   Copyright 2006-2008 SpringSource (http://springsource.com), All Rights Reserved
+# Copyright 2006-2011, SpringSource - http://springsource.com
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+# __all__ = []
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.       
-"""
-
+# import std
 import logging
+# import third party
+# import local
 from springpython.context import scope
+
 
 class ObjectContainer(object):
     """
@@ -111,7 +117,6 @@ class ObjectContainer(object):
         return dict([(key, kwargs[key].get_value(self)) for key in kwargs
                      if hasattr(kwargs[key], "get_value")])
 
-
     def _create_object(self, object_def):
         """
         If the object isn't stored in any scoped cache, and must instead be created, this method
@@ -132,12 +137,13 @@ class ObjectContainer(object):
         [prop.set_value(obj, self) for prop in object_def.props if hasattr(prop, "set_value")]
         
         return obj
-        
-        
+
+
 class AbstractObjectException(Exception):
     """ Raised when the user's code tries to get an abstract object from
     the container.
     """
-    
+
+
 class InvalidObjectScope(Exception):
     pass
